852f338770c115db49d83e4e7e86276605ccfc68
TuriniMattia <turinimattia96@gmail.com>
2024-04-11 15:27:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+  import Main from './components/Main.vue';
+
+  export default {
+    components: {
+      Main
+    },
+    data() {
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <Main />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
